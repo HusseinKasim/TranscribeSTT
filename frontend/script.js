0e799032faa57b7c7ef789a7f409ff2f1bcf2314
Hussein Kasim <husseinkasim20@yahoo.com>
2025-11-04 17:34:19 +0100

    Show ws response in textarea

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,16 @@
 const mic_btn = document.querySelector(".record_button")
+const txt_area = document.querySelector(".textarea")
+
 let chunks = []; // Recorded audio data
 let is_recording = false;
 
 mic_btn.addEventListener("click", ToggleMic);
 
+let ws;
+
 // Process input mic audio
 async function CollectSteam(){
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-        
         // Create AudioContext instance
         const audioContext = new AudioContext();
 
@@ -21,34 +24,54 @@ async function CollectSteam(){
         await audioContext.audioWorklet.addModule('processor.js');
 
         // Create AudioWorkletNode in AudioContext (uses custom processor)
-        const micNode = new AudioWorkletNode(audioContext, 'mic-processor');
+        micNode = new AudioWorkletNode(audioContext, 'mic-processor');
 
         // Connect source to AudioWorkletNode to be processed
         source.connect(micNode);
 
         // Implement Websocket 
-        const ws = new WebSocket("ws://localhost:8002/api/data")
+        ws = new WebSocket("ws://localhost:8002/api/data");
+        ws.onopen = () => {
 
-        // Fetch processed mic data as chunks from AudioWorkletNode processor
-        micNode.port.onmessage = (event) => {
-            // Send chunks to backend via Websocket 
-            ws.send(JSON.stringify({sample_rate: audioContext.sampleRate, msg: event.data}));
-        };
+            // Fetch processed mic data as chunks from AudioWorkletNode processor
+            micNode.port.onmessage = (event) => {
+                
+                // Send chunks to backend via Websocket 
+                ws.send(JSON.stringify({sample_rate: audioContext.sampleRate, msg: event.data}));    
+            };
+        
+            // Clear textarea
+            txt_area.innerHTML = "";
 
-        // Return JSON message from backend
-        ws.onmessage = (event) => {
-            let responseData = JSON.parse(event.data)
-            console.log(responseData)
-        };
-    };
-}
+            // Return JSON message from backend
+            ws.onmessage = (event) => {
+                let response = JSON.parse(event.data)['message'];
+                
+                // Filter out spaces
+                if(response && response.includes(' '))
+                {
+                    response = response.replace(/\s/g, '').trim();    
+                }
+                
+                // Filter out |
+                if(response && response.includes("|"))
+                {
+                    response = response.replace(/\|/g, ' ') 
+                }
+
+                // Update textarea
+                txt_area.innerHTML += response;
+            };
+        };  
+    }
+};
 
 // Toggle mic recording functionality
 async function ToggleMic(){
     is_recording = !is_recording;
     if(is_recording)
     {
-        await CollectSteam();
+        CollectSteam(is_recording);
         console.log("Started");
     }
 }
