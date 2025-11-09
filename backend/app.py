f03304d266b1fc5e58fd625dea55710af48d0281
Hussein Kasim <husseinkasim20@yahoo.com>
2025-11-09 14:04:42 +0100

    Folder restructure to allow for GitHub Pages imlementation

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ buffer = []
 # Temp for dev
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = "http://127.0.0.1:8000/",
+    allow_origins = origins,
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
